edcbc1ca00e13156727a171d1a87c66ae1ca4dcf
Ankit Jain <ajain@willowinc.com>
2022-04-01 16:29:51 -0700

    Scrolling the table, formating the dates and currency

--- src/components/sales/Sales.js
+++ src/components/sales/Sales.js
@@ -4,38 +4,57 @@ import { useStackline } from "../../context/StacklineContext";
 import "./Sales.css";
 
 function Sales() {
-  const salesData = useStackline();
+  const apiData = useStackline();
+  const salesData = apiData[0].sales;
 
-  const filteredData = salesData[0].sales.slice(0, 10);
+  const currencyFormat = (num) => {
+    return (
+      "$" +
+      num
+        .toFixed(2)
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+        .slice(0, -3)
+    );
+  };
+
+  const changeDateFormat = (inputDate) => {
+    var splitDate = inputDate.split("-");
+    if (splitDate.count === 0) {
+      return null;
+    }
+    var year = splitDate[0];
+    var month = splitDate[1];
+    var day = splitDate[2];
+
+    return month + "-" + day + "-" + year;
+  };
 
   return (
-    <>
-      <div className="panel m-3">
-        <Table hover className="table">
-          <thead>
-            <tr>
-              <th>Week Ending</th>
-              <th>Retail Sales</th>
-              <th>Wholesale Sales</th>
-              <th>Units Sold</th>
-              <th>Retailer Margin</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredData &&
-              filteredData.map((sales) => (
-                <tr>
-                  <td>{sales.weekEnding}</td>
-                  <td>{sales.retailSales}</td>
-                  <td>{sales.wholesaleSales}</td>
-                  <td>{sales.unitsSold}</td>
-                  <td>{sales.retailerMargin}</td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
-      </div>
-    </>
+    <div className="panel m-3 salesTable">
+      <Table hover className="table">
+        <thead>
+          <tr>
+            <th>Week Ending</th>
+            <th>Retail Sales</th>
+            <th>Wholesale Sales</th>
+            <th>Units Sold</th>
+            <th>Retailer Margin</th>
+          </tr>
+        </thead>
+        <tbody>
+          {salesData &&
+            salesData.map((sales) => (
+              <tr>
+                <td>{changeDateFormat(sales.weekEnding)}</td>
+                <td>{currencyFormat(sales.retailSales)}</td>
+                <td>{currencyFormat(sales.wholesaleSales)}</td>
+                <td>{sales.unitsSold}</td>
+                <td>{currencyFormat(sales.retailerMargin)}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
